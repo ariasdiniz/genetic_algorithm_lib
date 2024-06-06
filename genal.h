f591b51d8c486a0b2afd1d9ccc904210ac210a39
Aria Diniz <aria.diniz.dev@gmail.com>
2024-06-06 13:35:13 -0300

    removing unused constants

--- genal.h
+++ genal.h
@@ -1,13 +1,6 @@
 #ifndef _ARIA_GENETIC_ALGORITHM_FUNCTIONS_H
 #define _ARIA_GENETIC_ALGORITHM_FUNCTIONS_H
 
-#define __MIN 0
-#define __MAX 1
-#define __NUMBER_OF_WEIGHTS 4
-#define __MUTATION_PROBABILITY 0.01
-#define __MUTATION_RANGE 0.0000001
-#define __REPRODUCTION_RATE 0.15
-
 typedef struct Individual {
   float weights[__NUMBER_OF_WEIGHTS];
   double fitness;
